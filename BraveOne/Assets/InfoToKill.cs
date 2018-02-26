502eeb9c2fa45578a22409c0ac2fbd298957cbb8
Benson <bepdesign5@gmail.com>
2018-02-26 01:54:01 -0500

    Enemy kill count UI

---
+++ BraveOne/Assets/InfoToKill.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoToKill : MonoBehaviour {
+
+	public Text info;
+	private float waitTime = 2f;
+	// Use this for initialization
+	void Start () {
+		info.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			info.enabled = true;
+			info.text = "Kill 5 Enemies to move on";
+				StartCoroutine(TextWait());
+			}
+
+	}
+	IEnumerator TextWait()
+	{
+		yield return new WaitForSeconds(waitTime);
+		info.enabled = false;
+		//Destroy(gameObject);
+	}
+}
