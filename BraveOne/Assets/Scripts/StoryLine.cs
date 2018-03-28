065f5392b7f86ab4712bd9a458a7cde528620852
Benson <bepdesign5@gmail.com>
2018-03-28 18:12:31 -0400

    Adding enemies and StoryLine

---
+++ BraveOne/Assets/Scripts/StoryLine.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryLine : MonoBehaviour {
+
+	public void LoadGame ()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+	}
+}
